fe69b048bd57f6ffcc025a562af19d178ed68ccc
Matheus-Broring <matheus1107.hqr@gmail.com>
2025-05-23 19:55:07 -0300

    Aula de fuções nativas e operações aritiméticas

---
+++ Aula python/Aula4.py
@@ -0,0 +1,38 @@
+# num1 = 0
+# num2 = 0
+
+# num1= int(input("Digite o primeiro número: "))
+# num2= int(input("Digite o segundo número: "))
+# som= num1+num2
+# sub= num1-num2
+# mul= num1*num2
+# div= num1/num2
+# print("################################################")
+# print("A soma dos número é: ", som)
+# print("A subtração dos dois números é:", sub)
+# print("A divisão dos dois números é:",div)
+# print("A multiplicação dos dois números é:", mul)
+
+# base=5
+# menos= base-1
+# mais=base+1
+# duplicado=base*2
+# metade=base/2
+# print("A base foi de ", base)
+# print("A base mais 1 é: ",mais)
+# print("A base menos 1 é: ", menos)
+# print("A base vezes dois é: ", duplicado)
+# print("A metade da base é: ", metade)
+
+#Desafio
+
+numq1=0.0
+numq2=0.0
+numq3=0.0
+
+numq1=float(input("Informe o primeiro número com 3 casas decimais, e utilizando '.' ao inves de ',': "))
+numq2=float(input("Informe o segundo número com 3 casas decimais, e utilizando '.' ao inves de ',': "))
+numq3=float(input("Informe o terceiro número com 3 casas decimais, e utilizando '.' ao inves de ',': "))
+
+media= (numq1+numq2+numq3)/3
+print(f"A média é de :", media)
---
+++ Aula python/Aula5.py
@@ -0,0 +1,21 @@
+import math
+import os
+
+# x = 16
+# raiz= math.sqrt(x)
+
+# print("Raix quadrada de ", x ,"é igual a",raiz)
+
+# ang=45
+# sin= math.sin(ang)
+# print("O seno de", ang, "é :", sin)
+
+# dir=os.getcwdb()
+# print("O diretorio corrente é ",dir)
+
+lista=[10,20,30]
+tam= len(lista)
+print("A lista", lista, "tem tamanho", tam)
+
+soma= sum(lista)
+print("A lista", lista, "tem somatoria de", soma)
